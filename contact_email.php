365d10d6bbf62115e4be310adb71400ffd251237
GOTETI JAYACHANDRA MOHAN LAKSHMI NARASIMHA MURTHY <63448352+jayachandragoteti@users.noreply.github.com>
2020-08-02 10:18:28 +0530

    contact us updated

---
+++ contact_email.php
@@ -0,0 +1,23 @@
+<?PHP 
+if(isset($_POST['submit'])){
+ $Name=$_POST['Name'];
+ $email=$_POST['email'];
+ $subject=$_POST['subject'];
+ $massage=$_POST['massage'];
+  //--------------------- Mail -------------------  
+ $to ='korukondaexports@gmail.com';
+ $from =$email;
+ $message = " Hi  this is ". $Name.".
+ ". $massage." 
+ ";
+ $headers = "From:" . $from;
+ if (mail($to, $subject,$message,$headers)){
+         echo "<script>alert('Request submitted successfully')</script>";
+         echo "<script>window.location='contact.html';</script>";
+ }else{
+         echo "<script>alert(' Failed try again!')</script>";
+         echo "<script>window.location='contact.html';</script>";
+ }
+//--------------------- /Mail -------------------
+}
+?>
