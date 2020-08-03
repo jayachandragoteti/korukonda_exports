5d4c766bcc0989284f441876e943a0b893d3fcbe
GOTETI JAYACHANDRA MOHAN LAKSHMI NARASIMHA MURTHY <63448352+jayachandragoteti@users.noreply.github.com>
2020-08-03 22:10:27 +0530

    icons added

--- requests.php
+++ requests.php
@@ -4,21 +4,7 @@ if(isset($_POST['submit'])){
  $email=$_POST['email'];
  $subject=$_POST['subject'];
  $massage=$_POST['massage'];
-  //--------------------- Mail -------------------  
- $to ='korukondaexports@gmail.com';
- $from =$email;
- $message = " Hi  this is ". $Name.".
- ". $massage." 
- ";
- $headers = "From:" . $from;
- if (mail($to, $subject,$message,$headers)){
-         echo "<script>alert('Request submitted successfully')</script>";
-         echo "<script>window.location='contact.html';</script>";
- }else{
-         echo "<script>alert(' Failed try again!')</script>";
-         echo "<script>window.location='contact.html';</script>";
- }
-//--------------------- /Mail -------------------
+
 }
 if (isset($_POST['Enquiry_Now'])) {
         $name=$_POST['name'];
@@ -27,9 +13,22 @@ if (isset($_POST['Enquiry_Now'])) {
         $email=$_POST['email'];
         $requirements=$_POST['requirements'];
         $content=$_POST['content'];
-
+  //--------------------- Mail -------------------  
+  $to ='korukondaexports@gmail.com';
+  $from =$email;
+  $message = " Hi  this is ". $Name.".
+  ". $massage." 
+  ";
+  $headers = "From:" . $from;
+  if (mail($to, $subject,$message,$headers)){
         echo "<script>alert('Request submitted successfully')</script>";
         echo "<script>window.location='idex.html';</script>";
+  }else{
+          echo "<script>alert(' Failed try again!')</script>";
+          echo "<script>window.location='idex.html';</script>";
+  }
+ //--------------------- /Mail -------------------
+       
 
 }
 
